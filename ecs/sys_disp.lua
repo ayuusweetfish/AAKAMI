c5d9e7f4eea05b43376f9beced47b16d3432eac8
Shiqing <shiqing-thu18@yandex.com>
2019-09-08 21:37:25 +0800

    :pencil: Round coordinates for displaying

--- ecs/sys_disp.lua
+++ ecs/sys_disp.lua
@@ -4,7 +4,11 @@ component = 'sprite',
 spritesheet = spritesheet,
 update = function (self, es)
     for _, e in pairs(es) do
-        self.spritesheet.draw(e.sprite.name, e.dim[1], e.dim[2])
+        self.spritesheet.draw(
+            e.sprite.name,
+            math.floor(e.dim[1] + 0.5),
+            math.floor(e.dim[2] + 0.5)
+        )
     end
 end
 
