a53ac7dfa43b535462c8b260743fef84fbe89515
Shiqing <shiqing-thu18@yandex.com>
2019-09-12 16:55:26 +0800

    :bug: Fix bullets can hit non-blocking colliders

--- ecs/sys_bullet.lua
+++ ecs/sys_bullet.lua
@@ -4,6 +4,7 @@ update = function (self, cs)
     for _, e1 in pairs(cs.bullet or {}) do
         for _, e2 in pairs(cs.colli) do
             if e2 ~= e1 and e2 ~= e1.bullet.source
+                and (e2.colli.block or e2.player)
                 and rectIntsc(e1.dim, e2.dim)
             then
                 e1._removal = true
